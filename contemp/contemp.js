cd29ecc46445333ac31d971d67a443f2253eeddf
RafaelLeal0 <rleal3105@gmail.com>
2024-10-31 15:45:59 -0300

    contemporanea

---
+++ contemp/contemp.js
@@ -0,0 +1,8 @@
+const menuToggle = document.getElementById('menuToggleCustom'); 
+
+const sidebar = document.getElementById('sidebarCustom'); 
+menuToggle.addEventListener('click', function() { 
+
+    sidebar.classList.toggle('active'); 
+
+}); 
