ed0ecb9e7c691108aedb113d3f940c26ad39a7a6
AndreyMonti <andreymontibeller02@gmail.com>
2024-10-31 11:58:47 -0300

    testeconeclogout

--- logout.php
+++ logout.php
@@ -1,8 +1,20 @@
 <?php
+// Inicia a sessão
 session_start();
+
+// Remove todas as variáveis de sessão
 session_unset();
+
+// Destroi a sessão
 session_destroy();
 
+// Impede o armazenamento em cache para evitar o uso da seta de "voltar"
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+
+// Redireciona o usuário para a página inicial ou de login
 header("Location: index.php");
 exit();
 ?>
