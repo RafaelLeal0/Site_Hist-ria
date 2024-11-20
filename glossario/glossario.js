7621705a4837599479874208e44b9a6d12b84a9b
Samu3lb0az <samuelboaz6@gmail.com>
2024-11-20 13:58:57 -0300

    carrossel criado e redirecionado

--- glossario/glossario.js
+++ glossario/glossario.js
@@ -3,3 +3,13 @@ document.getElementById('menuToggle').addEventListener('click', function() {
     sidebar.classList.toggle('active');
 });
 
+
+function moveCarousel(direction) {
+    const carousel = document.querySelector('.alfabeto_carrossel');
+    const scrollAmount = carousel.offsetWidth / 5; // Mover uma fração do carrossel
+    carousel.scrollBy({
+        left: direction * scrollAmount,
+        behavior: 'smooth'
+    });
+}
+
